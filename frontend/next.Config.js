9521589cc7ac2a553cf9248a1231778be79a678d
huong <59523300+imhuog@users.noreply.github.com>
2025-07-31 10:15:17 +0700

    Update next.Config.js

--- frontend/next.Config.js
+++ frontend/next.Config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true, // Khuyến nghị bật strict mode cho React
   // Xóa hoặc đảm bảo không có 'swcMinify: true' ở đây
   // Nếu bạn có các cấu hình khác, hãy giữ lại chúng ngoại trừ swcMinify
+ env: {
+    NEXT_PUBLIC_SOCKET_URL: 'https://othello-huong.onrender.com', // 👈 sửa đúng URL backend thật của Meow nha
+  },
+  
 };
 
 module.exports = nextConfig;
